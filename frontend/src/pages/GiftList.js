fa883ec188c7eff1f25ad353974010bd60caa1e7
김호성 <dsrla123@naver.com>
2023-01-26 16:58:07 +0900

    feat: css 기본 수정

--- frontend/src/pages/GiftList.js
+++ frontend/src/pages/GiftList.js
@@ -9,18 +9,18 @@ const GiftList = () =>{
         <div>
             <h1> Home</h1>
             
-            <img src='https://www.naver.com' height={100} width={100} /> 
-            <img src='https://www.naver.com' height={100} width={100} /> 
-            <img src='https://www.naver.com' height={100} width={100} /> 
-            <br/>
-            <img src='https://www.kakao.com' height={100} width={100} /> 
-            <img src='https://www.naver.com' height={100} width={100} /> 
-            <img src='https://www.naver.com' height={100} width={100} /> 
-            <br/>
-            <Link to="/gifts/:giftid"><img src='https://shop2.daumcdn.net/thumb/R500x500/?fname=http%3A%2F%2Fshop2.daumcdn.net%2Fshophow%2Fp%2FZ20293623281.jpg%3Fut%3D20221209092821' height={100} width={100} /></Link>
-            <Link to="/gifts/:giftid"><img src='http://www.akbobada.com/home/akbobada/archive/akbo/img/202212200942036.jpg' height={100} width={100} /></Link>
-            <Link to="/gifts/:giftid"><img src='https://image.bugsm.co.kr/album/images/200/40824/4082425.jpg?version=20230105002539.0' height={100} width={100} /></Link>
-            <br/>
+            <Link to="/gifts/:giftid"><img src='http://www.akbobada.com/home/akbobada/archive/akbo/img/202212200942036.jpg' height={105} width={170} /></Link>
+            <Link to="/gifts/:giftid"><img src='http://www.akbobada.com/home/akbobada/archive/akbo/img/202212200942036.jpg' height={105} width={170} /></Link>
+            <Link to="/gifts/:giftid"><img src='http://www.akbobada.com/home/akbobada/archive/akbo/img/202212200942036.jpg' height={105} width={170} /></Link>
+   
+            <Link to="/gifts/:giftid"><img src='http://www.akbobada.com/home/akbobada/archive/akbo/img/202212200942036.jpg' height={105} width={170} /></Link>
+            <Link to="/gifts/:giftid"><img src='http://www.akbobada.com/home/akbobada/archive/akbo/img/202212200942036.jpg' height={105} width={170} /></Link>
+            <Link to="/gifts/:giftid"><img src='http://www.akbobada.com/home/akbobada/archive/akbo/img/202212200942036.jpg' height={105} width={170} /></Link>
+      
+            <Link to="/gifts/:giftid"><img src='https://shop2.daumcdn.net/thumb/R500x500/?fname=http%3A%2F%2Fshop2.daumcdn.net%2Fshophow%2Fp%2FZ20293623281.jpg%3Fut%3D20221209092821' height={105} width={170} /></Link>
+            <Link to="/gifts/:giftid"><img src='http://www.akbobada.com/home/akbobada/archive/akbo/img/202212200942036.jpg' height={105} width={170} /></Link>
+            <Link to="/gifts/:giftid"><img src='https://image.bugsm.co.kr/album/images/200/40824/4082425.jpg?version=20230105002539.0' height={105} width={170} /></Link>
+            
             {/* <BrowserRouter>
             <Routes>
                 <Route exact path="/" element={<Home/>}/>
