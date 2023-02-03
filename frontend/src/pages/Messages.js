285cd871d1d16590ec8fed4ac9013446e3217999
김호성 <dsrla123@naver.com>
2023-02-03 12:11:16 +0900

    feat: ¸޽ÅÀú ³»ºÎ

--- frontend/src/pages/Messages.js
+++ frontend/src/pages/Messages.js
@@ -1,47 +1,92 @@
 import React from 'react';
 import styled from 'styled-components';
 import Headset from '../assets/images/Group.png'
+import Send from '../assets/images/send.png'
 const StyledMessageBox=styled.div`
     display:flex;
     flex-direction:column;
     background-color:black;
     justify-content:center;
-    align-items:center;
+    // align-items:center;
 
 `
 const StyledMyMessage=styled.div`
     display:flex;
+    justify-content:end;
+    height:50px;
+    margin-right:16px;
 `
 
 const StyledMessage=styled.div`
     display: flex;
+    background-color: '#F5336D';
+    heigth:50px;
+    margin-left:16px;
 `
 const StyledHeadsetImage=styled.img`
     background-color:white;
-    width: 32px;
-    heigth: 32px;
+    width: 42px;
+    heigth: 42px;
     border-radius:100%;
+ 
 `
 const StyledInput=styled.input`
     placeholder: "새로운 사람과의 대화를 시작합니다";
-    size:text;
+    type:text;
     width: 90vw;
+    background-color: #1E0E40;
+`
+const StyledOpponent=styled.div`
+    background-color: #1E0E40;
+    height: 50px;
+    margin: 24px ;
+    display: flex;
+    align-items: center;
+`
+
+const StyledInputSet=styled.div`
+    background-color; #1E0E40;
+    display:flex;
+
+`
+const StyledSendImage=styled.img`
+    width:20px;
+    heigth:20px;
+    
+`
+const StyledSentMessage=styled.div`
+    border: 1px solid #6522F2;
+    border-radius: 15px 0px 15px 15px;
+`
+
+const StyledGotMessage=styled.div`
+    background-color: #6522F2;
+    border-radius: 0px 15px 15px 15px;
 `
 
 const Messages = () => {
     return (
+        <>
         <StyledMessageBox>
-        메시지    
+        <StyledOpponent>대화상대님과의 편지</StyledOpponent>    
         
         <StyledMyMessage>
-            <div >내가 보낸 메시지</div><StyledHeadsetImage src={Headset}/>
+            <StyledSentMessage >내가 보낸 메시지</StyledSentMessage><StyledHeadsetImage src={Headset}/>
         </StyledMyMessage>
         <StyledMessage>
-            <StyledHeadsetImage src={Headset}/><div background-color='#F5336D'>받은 메시지</div>
+            <StyledHeadsetImage src={Headset}/>
+            <div>
+                보낸사람    
+            <StyledGotMessage >받은 메시지</StyledGotMessage>
+            </div>
+            
         </StyledMessage>
 
-        <StyledInput />
+        
         </StyledMessageBox>
+        <hr/>
+        <StyledInputSet><form><StyledInput /></form> <StyledSendImage src={Send}/></StyledInputSet>
+        </>
     );
 };
 
