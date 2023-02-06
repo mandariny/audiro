7e8bf9fdc294669d10521fbf981745be8b685ee9
gaok <dlrkdhr321@daum.net>
2023-02-07 00:32:24 +0900

    fix: AWS 오류 해결을 위한 Repository 테이블명 수정

--- backend/audiro/src/main/java/com/a402/audiro/repository/GiftRepository.java
+++ backend/audiro/src/main/java/com/a402/audiro/repository/GiftRepository.java
@@ -12,13 +12,13 @@ public interface GiftRepository extends Repository<Gift, Long> {
     Gift findById(long id);
     List<Gift> findByUserId(long userId);
 
-    @Query(value = "select gift_img from gift where song_id = :song_id and spot_id=:spot_id", nativeQuery = true)
+    @Query(value = "select gift_img from Gift where song_id = :song_id and spot_id=:spot_id", nativeQuery = true)
     List<String> findBySongId(@Param("song_id") long songId, @Param("spot_id") long spotId);
 
-    @Query(value = "select * from gift join user on gift.user_id = user.user_id where user.nickname = :nickname", nativeQuery = true)
+    @Query(value = "select * from Gift join User on Gift.user_id = User.user_id where User.nickname = :nickname", nativeQuery = true)
     List<Gift> findByNickname(@Param("nickname") String nickname);
 
-    @Query(value = "select * from gift where gift.is_manito = TRUE",
+    @Query(value = "select * from Gift where Gift.is_manito = TRUE",
             nativeQuery = true)
     List<Gift> findManitos();
 
--- backend/audiro/src/main/java/com/a402/audiro/repository/SongMetaRepository.java
+++ backend/audiro/src/main/java/com/a402/audiro/repository/SongMetaRepository.java
@@ -13,19 +13,19 @@ public interface SongMetaRepository extends Repository<SongMeta, Long> {
 
     void save(SongMeta songMeta);
 
-    @Query(value = "select * from song_meta where song_meta.song_id = :song_id and song_meta.spot_id = :spot_id",
+    @Query(value = "select * from SongMeta where SongMeta.song_id = :song_id and SongMeta.spot_id = :spot_id",
             nativeQuery = true)
     SongMeta findBySongIdAndSpotId(@Param("song_id") long SongId, @Param("spot_id")long SpotId);
 
-    @Query(value = "select * from song_meta join song on song_meta.song_id=song.song_id where song_meta.spot_id = :spot_id order by gift_cnt DESC limit 10",
+    @Query(value = "select * from SongMeta join Song on SongMeta.song_id=song.song_id where SongMeta.spot_id = :spot_id order by gift_cnt DESC limit 10",
             nativeQuery = true)
     List<SongMeta> findBySpotIdByGiftCnt(@Param("spot_id")long SpotId);
 
-    @Query(value = "select * from song_meta join song on song_meta.song_id=song.song_id where song_meta.spot_id = :spot_id order by update_time DESC limit 10",
+    @Query(value = "select * from SongMeta join song on SongMeta.song_id=song.song_id where SongMeta.spot_id = :spot_id order by update_time DESC limit 10",
             nativeQuery = true)
     List<SongMeta> findBySpotIdByTime(@Param("spot_id")long SpotId);
 
-    @Query(value = "select * from song_meta join song on song_meta.song_id=song.song_id where song_meta.spot_id = :spot_id order by RAND() limit 10",
+    @Query(value = "select * from SongMeta join Song on SongMeta.song_id=Song.song_id where SongMeta.spot_id = :spot_id order by RAND() limit 10",
             nativeQuery = true)
     List<SongMeta> findBySpotIdByRandom(@Param("spot_id")long SpotId);
 
