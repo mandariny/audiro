5cb869d8968734a45e662e07cc9925fe3a319eae
윤소희 <elvmeldj@naver.com>
2023-02-07 08:51:42 +0900

    fix: SongRepository, SpotRepository 오류 수정

--- backend/audiro-postcard/src/main/java/com/a402/audiropostcard/repository/SpotRepository.java
+++ backend/audiro-postcard/src/main/java/com/a402/audiropostcard/repository/SpotRepository.java
@@ -2,10 +2,11 @@ package com.a402.audiropostcard.repository;
 
 import com.a402.audiropostcard.entity.Spot;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 
-public interface SpotRepository<Spot, Long> {
+public interface SpotRepository extends Repository<Spot, Long> {
 
     @Query(value = "select spot_id, name from Spot where spot_id = :spot_id", nativeQuery = true)
-    com.a402.audiropostcard.entity.Spot findById(@Param("song_id") long id);
+    Spot findById(@Param("song_id") long id);
 }
