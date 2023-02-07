cd481a93cf134a708f455ad90874a864d67316e8
윤소희 <elvmeldj@naver.com>
2023-02-07 19:36:37 +0900

    feat: mattermost 채널과 연동, 에러 알림 전달

--- backend/audiro/src/main/java/com/a402/audiro/AudiroApplication.java
+++ backend/audiro/src/main/java/com/a402/audiro/AudiroApplication.java
@@ -3,8 +3,11 @@ package com.a402.audiro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.web.client.RestTemplate;
 
 @EnableRetry
 @SpringBootApplication
@@ -12,6 +15,11 @@ public class AudiroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AudiroApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
