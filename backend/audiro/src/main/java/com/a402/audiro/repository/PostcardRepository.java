de71c492bea407661a429d6b5f8ea66600a3b31b
윤소희 <elvmeldj@naver.com>
2023-02-07 10:47:32 +0900

    feat: PasswordDTO, PostcardDTO 구현

---
+++ backend/audiro/src/main/java/com/a402/audiro/repository/PostcardRepository.java
@@ -0,0 +1,15 @@
+package com.a402.audiro.repository;
+
+import com.a402.audiro.entity.Postcard;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+
+public interface PostcardRepository extends Repository<Postcard, Long> {
+    @Query(value = "select * from postcard where password = :password", nativeQuery = true)
+    Postcard findByPasswrod(@Param("password") String pw);
+
+    Postcard findById(long id);
+
+    void save(Postcard postcard);
+}
