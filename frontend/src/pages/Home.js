9e0926a2c05236a6feae777b74f3348c390d76c3
gaok <dlrkdhr321@daum.net>
2023-02-08 15:37:12 +0900

    feat: 로그인후 queryString으로 token 받기

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState, useParams} from 'react';
 import Logo from '../components/Logo';
 import Nav from '../components/Nav';
 
-const Home = () => {
+const Home = (location) => {
+
+    const [token, setToken] = useState();
+
+    useEffect(() => {
+        const params = new URLSearchParams(location.search);
+        // console.log(window.location);
+        const jwtToken = params.get("auth");
+        // console.log(jwtToken);
+        setToken(jwtToken);
+    }, [])
 
     return (
         <div>
             <Logo/>
             <Nav/>
             홈 서비스 소개
+            {token}
         </div>
     );
 };
