f1e2aa43e5d2ccd7d3497fe1028114fa28a66151
gaok <dlrkdhr321@daum.net>
2023-02-09 13:55:08 +0900

    feat: 음악메이트 목록 컴포넌트 생성

---
+++ frontend/src/components/musicmate/MusicmateItem.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const StyledMMWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: rgba(65, 22, 162, 0.5);
+    margin-bottom: 10px;
+    width: 300px;
+    height: 50px;
+    padding-left: 20px;
+`;
+
+const StyledMMInfoWrapper=styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const StyledMMImg=styled.div`
+    border-radius: 100%;
+    background-color: white;
+    width: 30px;
+    height: 30px;
+    margin-right: 15px;
+`;
+
+const StyledMMNinkname=styled.div`
+    text-align: end;
+    color: white;
+    font-size: 15px;
+    font-family: var(--font-nanumSquareR);
+`;
+
+const StyledMMBlockBtn=styled.div`
+    text-align: end;
+    color: white;
+    font-size: 14px;
+    font-family: var(--font-nanumSquareR);
+    margin-right: 20px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    padding-left: 10px;
+    padding-right: 10px;
+    border-radius: 20px;
+    border: 2px solid #F5336D;
+`;
+
+const MusicmateItem = () => {
+    return (
+            <StyledMMWrapper>
+                <StyledMMInfoWrapper>
+                    <StyledMMImg></StyledMMImg>
+                    <StyledMMNinkname>닉네임</StyledMMNinkname>
+                </StyledMMInfoWrapper>
+                <StyledMMBlockBtn>차단하기</StyledMMBlockBtn>
+            </StyledMMWrapper>
+    );
+};
+
+export default MusicmateItem;
--- frontend/src/pages/Musicmate.js
+++ frontend/src/pages/Musicmate.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+import MusicmateItem from '../components/musicmate/MusicmateItem';
+import Logo from "../components/Logo";
+import Nav from "../components/Nav";
+
+import axios from 'axios';
+import ProfileHeader from "../components/mygift/ProfileHeader";
+import jwt from 'jwt-decode';
+
+const StyledMMContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin: 20px;
+`;
 
 const Musicmate = () => {
+
+    const token = localStorage.getItem('login-token');
+    console.log(jwt(token));
+    const nickname = jwt(token)['nickName']; 
+    console.log(nickname);
+
     return (
         <div>
-            내 메이트
+            <Logo/>
+            <Nav/>
+            <StyledMMContainer>
+                <MusicmateItem/>
+            </StyledMMContainer>
         </div>
     );
 };
