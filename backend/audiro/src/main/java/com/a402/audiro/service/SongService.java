0a90da0fc556a87bfd3b9986776ce51c22114b17
gaok <dlrkdhr321@daum.net>
2023-02-12 22:33:58 +0900

    Revert "feat: 노래 검색 기능 구현"
    This reverts commit ab998ea53d3f68ac857d88eed1534184cc768fed.

--- backend/audiro/src/main/java/com/a402/audiro/service/SongService.java
+++ backend/audiro/src/main/java/com/a402/audiro/service/SongService.java
@@ -1,11 +1,8 @@
 package com.a402.audiro.service;
 
-import java.util.List;
 import com.a402.audiro.entity.Song;
-import com.a402.audiro.dto.SongDTO;
 
 public interface SongService {
 
-    List<SongDTO> search(String keyword);
     Song isValidSong(long id);
 }
