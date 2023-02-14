636127268c66c199cd93877a6d8311eb65cf348f
윤소희 <elvmeldj@naver.com>
2023-02-14 23:42:24 +0900

    feat: 뮤직메이트 팔로우, 언팔로우 기능 구현

--- backend/audiro/src/main/java/com/a402/audiro/service/MusicmateServiceImpl.java
+++ backend/audiro/src/main/java/com/a402/audiro/service/MusicmateServiceImpl.java
@@ -3,7 +3,10 @@ package com.a402.audiro.service;
 import com.a402.audiro.dto.GiftThumbnailDTO;
 import com.a402.audiro.dto.MusicmateDTO;
 import com.a402.audiro.dto.MusicmateListDTO;
+import com.a402.audiro.entity.Musicmate;
 import com.a402.audiro.entity.User;
+import com.a402.audiro.exception.MusicmateAlreadyExistException;
+import com.a402.audiro.exception.MusicmateNotExistException;
 import com.a402.audiro.repository.MusicmateRepository;
 import com.a402.audiro.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +23,8 @@ public class MusicmateServiceImpl implements MusicmateService{
 
     private final MusicmateRepository musicmateRepository;
     private final UserRepository userRepository;
+    private final UserService userService;
+
 
     @Override
     public List<MusicmateListDTO> getMusicmateList(long userId) {
@@ -42,6 +47,44 @@ public class MusicmateServiceImpl implements MusicmateService{
         }
 
         return musicmateList;
+    }
+
+    @Override
+    public void followMusicmate(long mateId) {
+        User user = userService.getUser();
+        User mate = userService.getUser(mateId);
+
+        isAlreadyMusicmate(user, mate);
+
+        Musicmate musicmate = Musicmate.builder()
+                .user(user)
+                .mateUser(mate)
+                .build();
+
+        musicmateRepository.save(musicmate);
+    }
+
+    @Override
+    public void isAlreadyMusicmate(User user, User mate) {
+        Musicmate musicmate = musicmateRepository.findByUserIdAndMateId(user.getId(), mate.getId());
+        if(musicmate != null) throw new MusicmateAlreadyExistException();
+    }
+
+    @Override
+    public Musicmate getMusicmate(User user, User mate) {
+        Musicmate musicmate = musicmateRepository.findByUserIdAndMateId(user.getId(), mate.getId());
+        if(musicmate == null) throw new MusicmateNotExistException();
+        return musicmate;
+    }
+
+    @Override
+    public void unfollowMusicmate(long mateId) {
+        User user = userService.getUser();
+        User mate = userService.getUser(mateId);
+
+        Musicmate musicmate = getMusicmate(user, mate);
+
+        musicmateRepository.delete(musicmate);
     }
 
 }
