3b0ba2f1f3d2ee2e949d6b1e35435cb54a18e441
윤소희 <elvmeldj@naver.com>
2023-02-16 14:49:18 +0900

    feat: STOMP 인터셉터 설정

--- backend/audiro/src/main/java/com/a402/audiro/config/WebSocketConfig.java
+++ backend/audiro/src/main/java/com/a402/audiro/config/WebSocketConfig.java
@@ -1,14 +1,21 @@
 package com.a402.audiro.config;
 
+import com.a402.audiro.controller.StompHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompFrameHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
@@ -20,5 +27,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         config.enableSimpleBroker("/sub/spot/");
         config.setApplicationDestinationPrefixes("/pub/spot/");
     }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration){
+        registration.interceptors(stompHandler);
+    }
+
 
 }
